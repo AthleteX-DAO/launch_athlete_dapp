f1f5c5240cf656fa5095d2f8461d38ef08f6c6a4
Kevin Kamto <kamtokevin@gmail.com>
2022-02-08 17:18:07 -0500

    refactor: rewrote widgets

--- lib/main.dart
+++ lib/main.dart
@@ -23,15 +23,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'AX APTS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Launch an Athlete APT'),
@@ -42,15 +33,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -60,6 +42,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late Web3Client defaultClient;
   late Credentials defaultCredentials;
+  final _formKey = GlobalKey<FormState>();
   late BigInt expirationTimestamp, collateralPerPair, prepaidProposerReward;
   late String syntheticName, syntheticSymbol;
   late EthereumAddress collateralToken, financialProductLibrary, lspAddress;
@@ -141,126 +124,147 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.accessibility_new_rounded),
-                    title: Text("My Favorite Athlete"),
-                    subtitle: Text("Mint meee!"),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Enter the Synthetic Name",
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      setState(() {
-                        syntheticName = value;
-                      });
-                      return null;
-                    },
-                  ),
-                  DateTimePicker(
-                    type: DateTimePickerType.dateTimeSeparate,
-                    dateMask: 'd MMM, yyyy',
-                    initialValue: DateTime.now().toString(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                    icon: const Icon(Icons.event),
-                    dateLabelText: 'Date',
-                    timeLabelText: "Time",
-                    selectableDayPredicate: (date) {
-                      // Disable weekend days to select from the calendar
-                      return true;
-                    },
-                    onChanged: (val) {
-                      var parsedValue = DateTime.parse(val);
-                      setState(() {
-                        expirationTimestamp =
-                            BigInt.from(parsedValue.millisecondsSinceEpoch);
-                      });
-                    },
-                    validator: (val) {
-                      print(val);
-                      return null;
-                    },
-                    onSaved: (val) {
-                      var parsedValue = DateTime.parse(val!);
-                      setState(() {
-                        expirationTimestamp =
-                            BigInt.from(parsedValue.millisecondsSinceEpoch);
-                      });
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Enter the Synthetic Symbol",
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
+      body: Form(
+          key: _formKey,
+          child: Scrollbar(
+              child: Align(
+            alignment: Alignment.topCenter,
+            child: Card(
+              child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(20),
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 800),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ...[
+                          Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Card(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: <Widget>[
+                                      TextFormField(
+                                        decoration: const InputDecoration(
+                                          hintText: "Enter the Synthetic Name",
+                                        ),
+                                        validator: (String? value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter some text';
+                                          }
+                                          return null;
+                                        },
+                                        onChanged: (value) {
+                                          setState(() {
+                                            syntheticName = value;
+                                          });
+                                        },
+                                      ),
+                                      DateTimePicker(
+                                        type:
+                                            DateTimePickerType.dateTimeSeparate,
+                                        dateMask: 'd MMM, yyyy',
+                                        initialValue: DateTime.now().toString(),
+                                        firstDate: DateTime(2000),
+                                        lastDate: DateTime(2100),
+                                        icon: const Icon(Icons.event),
+                                        dateLabelText: 'Date',
+                                        timeLabelText: "Time",
+                                        selectableDayPredicate: (date) {
+                                          // Disable weekend days to select from the calendar
+                                          return true;
+                                        },
+                                        onChanged: (val) {
+                                          var parsedValue = DateTime.parse(val);
+                                          setState(() {
+                                            expirationTimestamp = BigInt.from(
+                                                parsedValue
+                                                    .millisecondsSinceEpoch);
+                                          });
+                                        },
+                                        validator: (val) {
+                                          print(val);
+                                          return null;
+                                        },
+                                        onSaved: (val) {
+                                          var parsedValue =
+                                              DateTime.parse(val!);
+                                          setState(() {
+                                            expirationTimestamp = BigInt.from(
+                                                parsedValue
+                                                    .millisecondsSinceEpoch);
+                                          });
+                                        },
+                                      ),
+                                      TextFormField(
+                                        decoration: const InputDecoration(
+                                          hintText:
+                                              "Enter the Synthetic Symbol",
+                                        ),
+                                        validator: (String? value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter some text';
+                                          }
 
-                      if (value.length > 3) {
-                        return "Please keep the symbol size short!";
-                      }
-                      setState(() {
-                        syntheticSymbol = value;
-                      });
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "How much collateral is needed?",
+                                          if (value.length > 3) {
+                                            return "Please keep the symbol size short!";
+                                          }
+                                          return null;
+                                        },
+                                        onChanged: (value) {
+                                          setState(() {
+                                            syntheticSymbol = value;
+                                          });
+                                        },
+                                      ),
+                                      TextFormField(
+                                        decoration: const InputDecoration(
+                                          hintText:
+                                              "How much collateral is needed?",
+                                        ),
+                                        validator: (String? value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter some text';
+                                          }
+                                          return null;
+                                        },
+                                        onChanged: (value) {
+                                          setState(() {
+                                            collateralPerPair =
+                                                BigInt.parse(value);
+                                          });
+                                        },
+                                      ),
+                                      ElevatedButton(
+                                          onPressed: () async {
+                                            bool response = await createAnAPT();
+                                            if (response) {
+                                              // Build success popup
+                                            } else {
+                                              // Build fail popup
+                                            }
+                                          },
+                                          child: const Icon(
+                                              Icons.account_balance_wallet))
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ]
+                      ],
                     ),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-
-                      setState(() {
-                        collateralPerPair = BigInt.parse(value);
-                      });
-                      return null;
-                    },
-                  ),
-                  ElevatedButton(
-                      onPressed: () async {
-                        bool response = await createAnAPT();
-                        if (response) {
-                          // Build success popup
-                        } else {
-                          // Build fail popup
-                        }
-                      },
-                      child: const Icon(Icons.account_balance_wallet))
-                ],
-              ),
+                  )),
             ),
-          ],
-        ),
-      ),
+          ))),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final eth = window.ethereum;
